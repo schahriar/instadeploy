35267a5e779b82231297d9b4a0d5aba2c3eafc02
Schahriar SaffarShargh <info@schahriar.com>
2015-10-05 15:07:20 -0700

    Removed non-existent error

--- instadeploy.js
+++ instadeploy.js
@@ -114,7 +114,7 @@ InstaDeploy.prototype.watch = function(directoryPath, remotePath) {
 		// Push File Data to SmartQueue
 		context.smartQueueList.push({ localPath: PATH, remotePath: path.join(remote, path.relative(directory, PATH)), callback: function(error) {
 			// If there is no upload error emit an uploaded event otherwise emit failed with error & path
-			if(!error) context.emit('uploaded', null, path.relative(directory, PATH), PATH);
+			if(!error) context.emit('uploaded', path.relative(directory, PATH), PATH);
 			else context.emit('failed', error, path.relative(directory, PATH), PATH);
 		}});
 	}, function FILE_ON_IGNORED(absolute, relative) {
