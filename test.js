67aae34e3e8c22186b760cee9bd3154d3fe71407
Schahriar SaffarShargh <info@schahriar.com>
2015-10-05 15:25:37 -0700

    Test Keys are now auto-generated
    A little slower than usual but there will be no private key in the test
    file.

--- test.js
+++ test.js
@@ -19,31 +19,7 @@ var OPEN_MODE = ssh2.SFTP_OPEN_MODE,
   STATUS_CODE = ssh2.SFTP_STATUS_CODE;
 var STORE_BUFFERS = {};
 
-var KEY = "-----BEGIN RSA PRIVATE KEY-----"
-  + "\nMIIEowIBAAKCAQEAlGMMUmn73JHeyZ2auaVhjxcVJOfKOrhGW2kjmGUmVIaBurrEmfyISWuv"
-  + "\n6fAqghICS+NM4Ln+rb1njC1M49pxjg25BBYZrW/myFc6wx84w+FWrtZo5xLU6+80FWHOEhNq"
-  + "\nM2tanig6BUQZsagVeqeFa5LH1tzLMUzZn+WctpINq8I4OsadQsUrosnZooOs1lqmeG7faAWk"
-  + "\nW87Vd1sR1H1G24k2Evz2Yn/jfjf8Tn8vLkGX7bPDL6as3Vh7xG9lJq+7eqGDHjgZ3J8prPsu"
-  + "\nbYvMgVsFfqUfAHn/YyfzRlfwp8b95L6zIGDxrkjduMt2ZNvZFZV2gxgRgAymt/RdvwRWuQID"
-  + "\nAQABAoIBAQCSR/6atE6NR+QoP82LXCUL6cZ3ZmWvc4Awlzp2/qhFxX/YdExiPVihgJ6f8ZoG"
-  + "\nNfi7lS1xkQonfsO4pePZ77voPVD6XUsc9ma9c3jDaGEOgXNx0n8B5Uev+1UOCygaG7ejTx67"
-  + "\nMY7ZKCRoRfo+uyzFOqL1Bx7s0ATuI25VcTA29C4UOCMR+wa1IC3VU+Ms6gjxyE4WGY370cwL"
-  + "\nuebcs08prQA/kYRxiMu5SBNKmcqH45b47JcrV3CjXYd67iIGv93RvVj6U39eZvygqIr/iKTm"
-  + "\nmzNLBGdKGYIAwVRA2U76+7K3D/db6Q/r/bGrX9qGhVgmDhhedFKhdQd8sNfMTxZdAoGBAOwu"
-  + "\naql+j61/+1NW+icWTwwyo+DDS3n+GTeK2AEl1QntXeBR5o9kFYJh+WiJrwihGlu+9TtGrJcT"
-  + "\nAIsNqaR/JRX3Ka4nhHC7T01C+EsJZRedacWpZB4Dcw6gDxvtH+TO5e/ABow6MQWbnfmdV/WG"
-  + "\nKoV8Kt2lUA6L9+Wv66zLdS6DAoGBAKDWp9noQDBDDqwfTUaeJFxB7bjL2oKovh/ux1feYkMh"
-  + "\nsCC5Eo0BTZ574tpWOL3491uUKgLaMNFueF+QlIsfZbLdT7KaVf+K/wYIZXuGJnGAI/oEr25p"
-  + "\nnTHrSFa0M6cS4+S+ulp3cZ+qVqzVzj5BlVJNTyrnL3I5FoN47AVr8SETAoGARG6qjxPRs5bK"
-  + "\njno8HwkyvDkMDLrpap56VgKP1b1NfgPd2HpCkLeSF+YlaunB4oEzbvPkgFlY9qkV1jSOO9Bv"
-  + "\nQW8ND535nORY9Oz7nlpJhU8h75jGHoAnXUx5NEE/pX9hVldQTl8qBxLw8ftqGgTW2zh//xrA"
-  + "\nGIbrvAx9/+IvwNsCgYAZJdbyzEyU/zchFHfZrcpTnn9T4JPW0BmCFqyWgY/tnSvTwfwzjduc"
-  + "\nBxTOPKL8mRvb9sumzITLijFKB+oh4pPdJptaeqUtoocDY3aynKQQJQ/6/JaNdff9ISObYuuc"
-  + "\nmOiHEIdEs95RL6oDth5cP5bgWnQhaipR+rkZb2O/6UO3fwKBgGkTxFKWKsMIyB3uepgpmebQ"
-  + "\nVdAA+oMqeRTksNnLyoZ6hXxwd7IvEhQu7vqd2CfeFfaQnIG9p0KDj4PMXlQ4/afMHsQL7Zm3"
-  + "\nQVc4f/KrAe2t2I0fl4pIFjzyReUy5lmgVnvG3fS4ZmOCozLNUlzxawW7oRdhE4jnit2keQ7D"
-  + "\nN1Nk"
-  + "\n-----END RSA PRIVATE KEY-----";
+var KEY = keypair().private;
 
 new ssh2.Server({
   privateKey: KEY
